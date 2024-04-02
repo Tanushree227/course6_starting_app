abce21be9efc4148391f3bad0055528d5bdfcd6f
Tanushree227 <tanushreedas813@gmail.com>
2024-04-02 19:28:56 +0530

    render a simple list component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 const fast_food = [
   {
-    "food": "pizza",
-    "province": "Italy"
+    id: '1',
+    food: "pizza",
+    province: "Italy"
   },
   {
-    "food": "hamburger",
-    "province": "Germany"
+    id: "2",
+    food: "hamburger",
+    province: "Germany"
   },
   {
-    "food": "Gol Gappa",
-    "province": "India"
+    id: "3",
+    food: "Gol Gappa",
+    province: "India"
   },
   {
-    "food": "Dumplings",
-    "province": "Japan"
+    id: "4",
+    food: "Dumplings",
+    province: "Japan"
   },
   {
-    "food": "Tacos",
-    "province": "Mexico"
+    id: "5",
+    food: "Tacos",
+    province: "Mexico"
   }
 ]
 
-const food = fast_food.map(f => {
-  return {
-    content: `${f.food}`
-  };
-})
-
-
 function App() {
-  console.log(food);
+  const items = fast_food.map(f => {
+    const text = `${f.food}`
+    return <li>{text}</li>
+  })
   return (
-    <div>
-      <h1 className="text-center bg-slate-800 font-bold text-white">Course 6 Starting App</h1>
+    <div className="bg-slate-800 text-white">
+      <h1 className="text-center font-bold">Course 6 Starting App</h1>
+      <ul>{items}</ul>
     </div>
   );
 }
