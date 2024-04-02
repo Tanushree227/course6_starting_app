93df58f9fa4ef4f2e2fefd02c5ccf8be0f5dcb91
Tanushree227 <tanushreedas813@gmail.com>
2024-04-02 19:18:41 +0530

    transforming lists in javascript

--- src/App.js
+++ src/App.js
@@ -1,4 +1,35 @@
+const fast_food = [
+  {
+    "food": "pizza",
+    "province": "Italy"
+  },
+  {
+    "food": "hamburger",
+    "province": "Germany"
+  },
+  {
+    "food": "Gol Gappa",
+    "province": "India"
+  },
+  {
+    "food": "Dumplings",
+    "province": "Japan"
+  },
+  {
+    "food": "Tacos",
+    "province": "Mexico"
+  }
+]
+
+const food = fast_food.map(f => {
+  return {
+    content: `${f.food}`
+  };
+})
+
+
 function App() {
+  console.log(food);
   return (
     <div>
       <h1 className="text-center bg-slate-800 font-bold text-white">Course 6 Starting App</h1>
