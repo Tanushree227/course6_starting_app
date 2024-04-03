3a8c303a9a2959c4d143a6a0da2e021d65dc0323
Tanushree227 <tanushreedas813@gmail.com>
2024-04-03 23:51:56 +0530

    useeffect hook

--- src/components/Week2.js
+++ src/components/Week2.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 export default function Week2() {
-    const [stateHook, setStateHook] = useState(null);
   return (
     <div className='my-10 shadow-lg p-5 bg-gray-400'>
         <h1 className='text-3xl font-bold text-pretty text-center'>Week 2: Getting Started with Hook</h1>
@@ -9,9 +8,9 @@ export default function Week2() {
             <p className='text-center text-3xl'>useState Hook</p>
             <p>In React, useState is a special function that lets you add state to functional components. It provides a way to declare and manage state variables directly within a function component. It should be noted that one use of useState() can only be used to declare one state variable. It was introduced in version 16.8.</p>
             <div>
-              <ul>
-                <p>Internal working of useState hook</p>
-                <li>creates a new cell in the functional component’s memory object.</li>
+              <ul className='list-item'>
+                <p className='font-bold'>Internal working of useState hook:</p>
+                <li className='list-item'>creates a new cell in the functional component’s memory object.</li>
                 <li>New state values are stored in this cell during renders.</li>
                 <li>The stack pointer points to the latest cell after each render.</li>
                 <li>Deliberate user refresh triggers stack dump and fresh allocation.</li>
@@ -19,7 +18,11 @@ export default function Week2() {
               </ul>
             </div>
         </div>
-        <p>{stateHook}</p>
+
+        <div>
+            <p className='text-center text-3xl'>useEffect Hook</p>
+            <p>The useEffect in ReactJS is used to handle the side effects such as fetching data and updating DOM. This hook runs on every render but there is also a way of using a dependency array using which we can control the effect of rendering</p>
+        </div>
     </div>
   )
 }
