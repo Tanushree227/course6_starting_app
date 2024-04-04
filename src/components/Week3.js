7628bd6a3b758330d68aa768fa236f9f76f55379
Tanushree227 <tanushreedas813@gmail.com>
2024-04-04 23:22:51 +0530

    bold -> medium

--- src/components/Week3.js
+++ src/components/Week3.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Week3() {
   return (
     <div className='shadow-lg p-5'>
-        <h1 className='text-3xl font-bold text-pretty text-center'>Week 3: JSX and Testing</h1>
+        <h1 className='text-3xl font-medium text-pretty text-center'>Week 3: JSX and Testing</h1>
     </div>
   )
 }
