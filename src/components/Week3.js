0f303a9c119c0ec1bada6e58cf028157df01b9d3
Tanushree227 <tanushreedas813@gmail.com>
2024-04-04 23:20:32 +0530

    jsx and testing

--- src/components/Week3.js
+++ src/components/Week3.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Week3() {
   return (
-    <div>Week3</div>
+    <div className='shadow-lg p-5'>
+        <h1 className='text-3xl font-bold text-pretty text-center'>Week 3: JSX and Testing</h1>
+    </div>
   )
 }
